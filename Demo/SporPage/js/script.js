f0c8dbf2895533f6242537cad8aa3e30b497b562
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2021-08-31 16:16:15 +0400

    Add files via upload

---
+++ Demo/SporPage/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    menuToggle.classList.toggle('active');
+    banner.classList.toggle('active');
+
+}
