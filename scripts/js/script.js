d9110c82baa75132443ebe64f68daa46c4ed826b
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2021-08-31 16:40:23 +0400

    Update script.js

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -35,16 +35,7 @@ window.addEventListener("load",()=>{
         var $item = $( image.img ).parents('.portfilo-item-thumbnail');
         $item.addClass('loaded');
     }
-    //LOAD EFFECT
-    var sr = ScrollReveal({
-        distance :'100px',
-        duration:2800,
-    });
-    sr.reveal(`.pih01, .pih02, .pih03, .pih04, .pih05, .pih06`,
-    {
-        origin:'bottom',
-        interval:100,
-    });
+
     
 });
 
