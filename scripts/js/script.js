2077dcc4aceae5020cb6da86b227eff931135f09
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2021-09-01 12:20:15 +0400

    Update script.js

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -218,9 +218,4 @@ function updateIcon(){
     }
 }
 
-var typed = new Typed('.typing-text',{
-    strings:['Javid Ismayil','A Font End developer','A Web Developer','A Web Designer'],
-    loop:true,
-    typeSpeed:150
-});
 
