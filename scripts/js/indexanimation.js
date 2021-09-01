e9898719cd2dbdc02dcce793ce81de2f916aae6a
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2021-09-01 12:21:58 +0400

    Update indexanimation.js

--- scripts/js/indexanimation.js
+++ scripts/js/indexanimation.js
@@ -21,3 +21,9 @@ function animation(){
 
 
 animation();
+
+var typed = new Typed('.typing-text',{
+    strings:['Javid Ismayil','A Font End developer','A Web Developer','A Web Designer'],
+    loop:true,
+    typeSpeed:150
+});
