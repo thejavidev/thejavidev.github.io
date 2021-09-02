e4fa86475a21c506e51a9f21115dccb6f25967d7
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2021-09-02 13:59:00 +0400

    Update indexanimation.js

--- scripts/js/indexanimation.js
+++ scripts/js/indexanimation.js
@@ -27,3 +27,16 @@ var typed = new Typed('.typing-text',{
     loop:true,
     typeSpeed:150
 });
+window.addEventListener("load",()=>{
+    $(function() {
+        $('#WAButton').floatingWhatsApp({
+          phone: '+994504702996',
+          headerTitle: 'Chat with us on WhatsApp!',
+          popupMessage: 'Hello, how can we help you?',
+          showPopup: true,
+          buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
+          position: "right"
+        });
+      });
+    
+})
