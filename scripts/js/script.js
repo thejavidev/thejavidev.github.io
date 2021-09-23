b67da1f40f0620082ca831dd110fa07ae6034559
Javid ISmayil <javidation.l@gmail.com>
2021-09-23 10:34:55 +0400

    thejavidev

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -179,34 +179,34 @@ function filterItems(filterBtn) {
 filterItems(document.querySelector(".portfilo-filter-btn.active"));
 
 /*--------------------dark theme start--------------------------------*/
-function toggleTheme() {
-  if (localStorage.getItem("Javid-theme") !== null) {
-    if (localStorage.getItem("Javid-theme") === "dark") {
-      $("body").addClass("dark");
-    } else {
-      $("body").removeClass("dark");
-    }
-  }
-  updateIcon();
-}
-toggleTheme();
+// function toggleTheme() {
+//   if (localStorage.getItem("Javid-theme") !== null) {
+//     if (localStorage.getItem("Javid-theme") === "dark") {
+//       $("body").addClass("dark");
+//     } else {
+//       $("body").removeClass("dark");
+//     }
+//   }
+//   updateIcon();
+// }
+// toggleTheme();
 
-$(".toggle-theme").on("click", function () {
-  $("body").toggleClass("dark");
-  if ($("body").hasClass("dark")) {
-    localStorage.setItem("Javid-theme", "dark");
-  } else {
-    localStorage.setItem("Javid-theme", "light");
-  }
-  updateIcon();
-});
+// $(".toggle-theme").on("click", function () {
+//   $("body").toggleClass("dark");
+//   if ($("body").hasClass("dark")) {
+//     localStorage.setItem("Javid-theme", "dark");
+//   } else {
+//     localStorage.setItem("Javid-theme", "light");
+//   }
+//   updateIcon();
+// });
 
-function updateIcon() {
-  if ($("body").hasClass("dark")) {
-    $(".toggle-theme i").removeClass("fa-moon");
-    $(".toggle-theme i").addClass("fa-sun");
-  } else {
-    $(".toggle-theme i").removeClass("fa-sun");
-    $(".toggle-theme i").addClass("fa-moon");
-  }
-}
+// function updateIcon() {
+//   if ($("body").hasClass("dark")) {
+//     $(".toggle-theme i").removeClass("fa-moon");
+//     $(".toggle-theme i").addClass("fa-sun");
+//   } else {
+//     $(".toggle-theme i").removeClass("fa-sun");
+//     $(".toggle-theme i").addClass("fa-moon");
+//   }
+// }
