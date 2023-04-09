53cceda810835dd6c7f591537e74d37dce046465
thejavidev <javidation.l@gmail.com>
2023-04-09 14:36:39 +0400

    upload1

---
+++ 3DSocialMedia/js/script.js
@@ -0,0 +1,18 @@
+let list = document.querySelectorAll('.sci li');
+let bg = document.querySelector('.socialMedia');
+list.forEach(elements =>{
+    elements.addEventListener('mouseenter',function(event){
+        let color = event.target.getAttribute('data-color');
+        bg.style.backgroundColor = color;
+    });
+    elements.addEventListener('mouseleave',function(event){
+        bg.style.backgroundColor = '#fff';
+    });
+});
+
+VanillaTilt.init(document.querySelectorAll('.sci li a'),{
+    max:30,
+    speed:400,
+    glare:true,
+    "max-glare":1,
+});
